dac1a3d12c67af5bafceff4df909a716d451e6a4
Santhoshkumar0077 <santhoshkumarmanoharan07@gmail.com>
2025-02-25 09:57:45 +0530

    css file changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import "./Navbar.css"; // Import the CSS file
+import "../styles/navbar.css"; // Import the CSS file
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
